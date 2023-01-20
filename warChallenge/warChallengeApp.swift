1655ff9ca4f456e07e6526c6ed055138e42ae158
gravilaxe <joshuawright3132@outlook.com>
2023-01-20 18:34:49 +0000

    Initial Commit

---
+++ warChallenge/warChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeApp.swift
+//  warChallenge
+//
+//  Created by Joshua Wright on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
